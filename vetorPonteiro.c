4d75e676ad3c457726cba0d3586ec21265cda256
Kaylanne Melo <85590863+KaeMelo@users.noreply.github.com>
2022-07-20 16:13:11 -0300

    Create vetorPonteiro.c

---
+++ vetorPonteiro.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main ()
+{
+    int *ptr_a;
+
+    ptr_a = malloc(sizeof(int));
+    ptr_a = (int*)realloc(ptr_a, sizeof(int));
+
+    if (ptr_a == NULL)
+    {
+        printf("Memória insuficiênte.\n");
+        exit(1);
+    }
+
+    printf("Endereço de ptr_a.\n");
+    *ptr_a = 22;
+    printf("Conteúdo do ptr_a.\n");
+
+    free(ptr_a);
+
+return 0;
+}
