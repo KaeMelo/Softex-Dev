48107e50e08b7ba4ab6222ba0f3515da045f5799
Kaylanne Melo <85590863+KaeMelo@users.noreply.github.com>
2022-08-10 14:19:48 -0300

    Create metodoGS.py

---
+++ metodoGS.py
@@ -0,0 +1,16 @@
+class Pessoa: 
+     def __init__(self, nome, corCabelo): 
+         self.nome = nome 
+         self.corCabelo = corCabelo
+  
+     def getNome(self): 
+         return self.nome 
+  
+     def setNome(self, nome): 
+         self.nome = nome 
+  
+     def getcorCabelo(self, corCabelo):
+         self.corCabelo = corCabelo
+  
+     def setcorCabelo(self, corCabelo): 
+         self.corCabelo = corCabelo
