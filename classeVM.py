cdfb9a25943e2b1b3b49aa4f67e8d55aceac13ec
Kaylanne Melo <85590863+KaeMelo@users.noreply.github.com>
2022-08-10 14:20:32 -0300

    Create classeVM.py

---
+++ classeVM.py
@@ -0,0 +1,16 @@
+class mercado:
+  def __init__(self, funcionarios, mediaSalarial):
+    self.funcionarios = funcionarios
+    self.mediaSalario = mediaSalarial
+
+  def qtdFuncionario(self):
+     quantidade = self.funcionario
+     print(quantidade)
+    
+   def qtdSalario(self):
+     salario = self.mediaSalario
+     print(salario)
+     
+objeto = mercado(20, 1500)
+print(objeto.qtdFuncionario())
+print(objeto.qtdSalario())
