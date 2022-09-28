9e09ba526c8d00161a4cd16e2c6a8b4245bb9911
Kaylanne Melo <85590863+KaeMelo@users.noreply.github.com>
2022-09-28 14:30:18 -0300

    Create forInforOf.js

---
+++ forInforOf.js
@@ -0,0 +1,11 @@
+const guitar = { color: "Green", brand: "Fender", model: "Stratocaster",
+}
+const car = ["Azera", "Hyundai", "2021"]
+
+for (const i in guitar) {
+  console.log(guitar[i]);
+}
+
+for (const x of car) {
+  console.log(x);
+}
