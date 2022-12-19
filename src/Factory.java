7cac91ac7fd83e245e3c50207412d12638aed697
Kaylanne Melo <85590863+KaeMelo@users.noreply.github.com>
2022-12-19 20:09:48 -0300

    Create Factory.java

---
+++ src/Factory.java
@@ -0,0 +1,11 @@
+public class Factory {
+
+    public static Computer createComputer(String type) {
+        if (type == "PC") {
+            return new PC();
+        } else if (type == "Server") {
+            return new Server();
+        }
+        return null;
+    }
+}
